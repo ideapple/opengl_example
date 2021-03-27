824053923bde0af181b9abd7f65f94e7401284b5
ideapple <ideapple0912@gmail.com>
2021-03-28 03:01:38 +0900

    commit
    4th Gradation Square

--- src/context.cpp
+++ src/context.cpp
@@ -9,39 +9,47 @@ ContextUPtr Context::Create() {
 
 bool Context::Init() {
 
-  float vertices[] = {
-  0.5f, 0.5f, 0.0f, // top right
-  0.5f, -0.5f, 0.0f, // bottom right
-  -0.5f, -0.5f, 0.0f, // bottom left
-  -0.5f, 0.5f, 0.0f, // top left
+ float vertices[] = {
+  0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f, // top right, white
+  0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, // bottom right, black
+  -0.5f, -0.5f, 0.0f, 0.3f, 0.7f, 0.4f, // bottom left, mint?
+  -0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, // top left, purple
 };
+
 uint32_t indices[] = { // note that we start from 0!
   0, 1, 3, // first triangle
   1, 2, 3, // second triangle
 };
+
 m_vertexLayout = VertexLayout::Create();
 
-m_vertexBuffer = Buffer::CreateWithData(
-  GL_ARRAY_BUFFER, GL_STATIC_DRAW,
-  vertices, sizeof(float) * 12);
-m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float) * 24);
 
-m_indexBuffer = Buffer::CreateWithData(
-  GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW,
-  indices, sizeof(uint32_t) * 6);
+//m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE,
+sizeof(float) * 6, 0);
+m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE,
+  sizeof(float) * 6, sizeof(float) * 3);
 
-ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
-ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
+m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(uint32_t) * 6);
+
+ShaderPtr vertShader = Shader::CreateFromFile("./shader/per_vertex_color.vs", GL_VERTEX_SHADER);
+ShaderPtr fragShader = Shader::CreateFromFile("./shader/per_vertex_color.fs", GL_FRAGMENT_SHADER);
 if (!vertShader || !fragShader)
     return false;
 SPDLOG_INFO("vertex shader id: {}", vertShader->Get());
 SPDLOG_INFO("fragment shader id: {}", fragShader->Get());
+
 m_program = Program::Create({fragShader, vertShader});
   if (!m_program)
     return false;
   SPDLOG_INFO("program id: {}", m_program->Get());
 
-  glClearColor(0.1f, 0.4f, 1.4f, 0.0f);
+  	
+auto loc = glGetUniformLocation(m_program->Get(), "color");
+m_program->Use();
+  glUniform4f(loc, 1.0f, 1.0f, 0.0f, 1.0f);
+  glClearColor(0.0f, 0.1f, 0.2f, 0.0f);
 
   return true;
 }
@@ -49,6 +57,14 @@ m_program = Program::Create({fragShader, vertShader});
 void Context::Render() {
   glClear(GL_COLOR_BUFFER_BIT);
 
+  /*static float time = 0.0f;
+  float t = sinf(time) * 0.5f + 0.5f;
+  auto loc = glGetUniformLocation(m_program->Get(), "color");*/
   m_program->Use();
+  //glUniform4f(loc, t*t, 2.0f*t*(1.0f-t), (1.0f-t)*(1.0f-t), 1.0f);
   glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+  //time += 0.016f;
+//  m_program->Use();
+// glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
